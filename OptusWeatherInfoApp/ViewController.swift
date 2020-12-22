8719581b76d95faccb7c49aafbc8f52d29c0d5d1
Sachin Ranadive <sr00604445@techmahindra.com>
2020-12-22 11:38:00 +0530

    Initial Commit

---
+++ OptusWeatherInfoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OptusWeatherInfoApp
+//
+//  Created by Sachin on 22/12/20.
+//  Copyright © 2020 Sachin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
